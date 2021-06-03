d64231aa39a1bf8b54cde043fbf1cc88db232b54
qinshuo <654989417@qq.com>
2021-06-03 10:28:19 +0800

    ..

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver import ActionChains
@@ -7,8 +8,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from PIL import Image
 import time
 import urllib.request
+import threading
 import random
 
 
@@ -35,22 +38,16 @@ def findPic(img_bg_path, img_slider_path):
     img_bg_gray = cv.cvtColor(img_bg, cv.COLOR_BGR2GRAY)
 
     # 读取滑块，参数1是图片路径，参数2是使用灰度模式
-    img_slider_gray = cv.imread(img_slider_path, 0)
+    img_slider = cv.imread(img_slider_path, 0)
+    cv.imshow('slider',img_slider)
+    cv.waitKey(100)
 
     # 在滑块背景图中匹配滑块。参数cv.TM_CCOEFF_NORMED是opencv中的一种算法
-    res = cv.matchTemplate(img_bg_gray, img_slider_gray, cv.TM_CCOEFF_NORMED)
+    res = cv.matchTemplate(img_bg_gray, img_slider, cv.TM_CCORR_NORMED)
 
     print('#' * 50)
     print(type(res))  # 打印：<class 'numpy.ndarray'>
     print(res)
-    # 打印：一个二维的ndarray数组
-    # [[0.05604218  0.05557462  0.06844381... - 0.1784117 - 0.1811338 - 0.18415523]
-    #  [0.06151756  0.04408009  0.07010461... - 0.18493137 - 0.18440475 - 0.1843424]
-    # [0.0643926    0.06221284  0.0719175... - 0.18742703 - 0.18535161 - 0.1823346]
-    # ...
-    # [-0.07755355 - 0.08177952 - 0.08642308... - 0.16476074 - 0.16210903 - 0.15467581]
-    # [-0.06975575 - 0.07566144 - 0.07783117... - 0.1412715 - 0.15145643 - 0.14800543]
-    # [-0.08476129 - 0.08415948 - 0.0949327... - 0.1371379 - 0.14271489 - 0.14166716]]
 
     print('#' * 50)
 
@@ -64,8 +61,46 @@ def findPic(img_bg_path, img_slider_path):
     return value[2:][0][0], value[2:][1][0]
 
 
+# 对滑块进行二值化处理
+def handle_img1(image):
+    kernel = np.ones((8, 8), np.uint8)  # 去滑块的前景噪声内核
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    width, heigth = gray.shape
+    for h in range(heigth):
+        for w in range(width):
+            if gray[w, h] == 0:
+                gray[w, h] = 96
+    # cv.imshow('gray', gray)
+    binary = cv.inRange(gray, 96, 96)
+    res = cv.morphologyEx(binary, cv.MORPH_OPEN, kernel)  # 开运算去除白色噪点
+    # cv.imshow('res', res)
+    return res
+
+
+# 模板匹配(用于寻找缺口有点误差)
+def template_match(img_target, img_template):
+    tpl = handle_img1(img_template)  # 误差来源就在于滑块的背景图为白色
+    blurred = cv.GaussianBlur(img_target, (3, 3), 0)  # 目标图高斯滤波
+    gray = cv.cvtColor(blurred, cv.COLOR_BGR2GRAY)
+    ret, target = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)  # 目标图二值化
+    # cv.imshow("template", tpl)
+    # cv.imshow("target", target)
+    method = cv.TM_CCOEFF_NORMED
+    width, height = tpl.shape[:2]
+    result = cv.matchTemplate(target, tpl, method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    left_up = max_loc
+    print(left_up)
+    # right_down = (left_up[0] + height, left_up[1] + width)
+    # cv.rectangle(img_target, left_up, right_down, (0, 0, 255), 2)
+    # cv.imshow('res', img_target)
+    # cv.waitKey(0)
+    return left_up[0]
+
+
 # 返回两个数组：一个用于加速拖动滑块，一个用于减速拖动滑块
 def generate_tracks(distance):
+    print("滑动距离：" + str(distance))
     # 给距离加上20，这20像素用在滑块滑过缺口后，减速折返回到缺口
     distance += 20
     v = 0
@@ -93,44 +128,44 @@ def main():
     # chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--disable-gpu')
 
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
 
     # 设置浏览器宽高
     driver.set_window_size(width=2000, height=3000)
 
     wait = WebDriverWait(driver, 10)
 
-    driver.get("https://sso.toutiao.com/")
+    driver.get("https://video.kuaishou.com/short-video/3x64va4fgudq9ec?fid=2157899965&cc=share_copylink&followRefer=151&shareMethod=TOKEN&docId=0&kpn=KUAISHOU&subBiz=PHOTO&shareResourceType=PHOTO_OTHER&userId=3xr5h4kk5he7fpy&photoId=3x64va4fgudq9ec&shareType=1&et=1_v/2000392675735970993_sff0&shareMode=APP&groupName=&appType=21&shareUrlOpened=0&utm_source=app_share&utm_medium=app_share&utm_campaign=app_share&location=app_share")
 
     try:
-        # 获取手机号码输入框
-        text_box = wait.until(expected_conditions.presence_of_element_located((By.ID, "user-mobile")))
-        print(text_box)
+        did = driver.get_cookie("did")
+        print(did)
 
-        # 获取发送验证按钮
-        send_btn = wait.until(expected_conditions.presence_of_element_located((By.ID, "mobile-code-get")))
-        print(send_btn)
+        xf = wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//iframe")))
+        driver.switch_to.frame(xf)
+
+        # image-container
+        container = wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//div[@class='image-container']")))
+        container.screenshot(r'./captcha1.png')
 
     except Exception as e:
         print(e)
         raise RuntimeError("获取 手机号码输入框 或者 发送验证按钮 失败，程序结束")
 
-    text_box.send_keys("18812345678")
 
-    # time.sleep(random.uniform(2, 3))
 
     try:
         action = ActionChains(driver)
-        action.click(send_btn).perform()
+
     except Exception as e:
         print(e)
     else:
-        time.sleep(1.5)
+        time.sleep(3)
 
         # 获取 滑块背景图
         bg_image = wait.until(
-            expected_conditions.presence_of_element_located((By.XPATH, '//div[@class="validate-main"]/img[1]')))
-        print("\n背景：")
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, "bg-img")))
+        print("\n背景：" + str(bg_image.size['width']))
 
         print(bg_image)
         bg_image_url = bg_image.get_attribute('src')
@@ -140,10 +175,13 @@ def main():
         # 使用urllib下载背景图
         # 原因是：使用bg_image.screenshot()程序卡在这里，也不报错
         urllib.request.urlretrieve(bg_image_url, "./img_bg.png")
+        bgim = Image.open('img_bg.png')
+        out = bgim.resize((bg_image.size['width'], bg_image.size['height']), Image.ANTIALIAS)
+        out.save('./img_bg.png')
 
         # 获取 滑块
         slider = wait.until(
-            expected_conditions.presence_of_element_located((By.XPATH, '//div[@class="validate-main"]/img[2]')))
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, "slider-img")))
 
         print("\n滑块：")
         print(slider)
@@ -152,20 +190,28 @@ def main():
         # 而截图截出的是矩形，会把滑块周围的滑块背景图一起截取，势必影响匹配
         # slider.screenshot('./img_slider.png')
         slider_url = slider.get_attribute('src')
+        print(slider_url)
 
         urllib.request.urlretrieve(slider_url, "./img_slider.png")
+        sliderim = Image.open('img_slider.png')
+        out = sliderim.resize((slider.size['width'], slider.size['height']), Image.ANTIALIAS)
+        out.save('./img_slider.png')
 
-        value_1, value_2 = findPic('./img_bg.png', './img_slider.png')
+        # value_1, value_2 = findPic('./img_bg.png', './img_slider.png')
+        img0 = cv.imread("./img_slider.png")
+        img1 = cv.imread("./img_bg.png")
+
+        value_2 = template_match(img1, img0)
 
         print("#" * 30)
-        print("最差匹配和最佳匹配对应的x坐标分别是：")
-        print(value_1)
+        print("最佳匹配对应的x坐标是：")
         print(value_2)
         print("#" * 30)
 
         # 获取滑动按钮
         button = wait.until(
-            expected_conditions.presence_of_element_located((By.XPATH, '//div[@class="validate-drag-button"]/img')))
+            expected_conditions.presence_of_element_located((By.XPATH, '//div[@class="slider-btn"]')))
+        print(button)
 
         action = ActionChains(driver)
         try:
@@ -179,22 +225,25 @@ def main():
 
         for x in forward_tracks:
             action.move_by_offset(x, 0)  # 前进移动滑块
-            print(x)
+            # print(x)
 
         print('#' * 50)
 
         for x in back_tracks:
             action.move_by_offset(x, 0)  # 后退移动滑块
-            print(x)
+            # print(x)
 
         action.release().perform()
 
 
     finally:
-        # time.sleep(10000)
+        time.sleep(10000)
         driver.close()
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    for i in range(10):
+        t = threading.Thread(target=main)
+        t.start()
 
